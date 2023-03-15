91b76ea761eb03c4347d8ebc84447ce83c76b47b
Nhat <50206162+NhatLg@users.noreply.github.com>
2023-03-15 10:23:30 +0100

    first commit, view is mostly implemented

---
+++ view.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+from tkinter import ttk
+
+class View(tk.Tk):
+    PAD = 10
+    MAX_BUTTONS_PER_ROW = 4
+    button_captions = [
+        'C', '+/-', '%', '/',
+        7, 8, 9, "*",
+        4, 5, 6, "-",
+        1, 2, 3, "+",
+        0, ".", "="
+    ]
+
+    def __init__(self, controller):
+        super().__init__()
+        self.controller = controller
+        self.title("Calculator_v1.0")
+
+        self.value_var = tk.StringVar()
+
+        self._make_mainframe()
+        self._make_entry()
+        self._make_buttons()
+
+    def main(self):
+        self.mainloop()
+
+    def _make_mainframe(self):
+        self.main_frm = ttk.Frame(self)
+        self.main_frm.pack(padx=self.PAD, pady=self.PAD)
+
+    def _make_entry(self):
+        ent = ttk.Entry(self.main_frm, textvariable=self.value_var, justify='right', state="disabled")
+        ent.pack(fill='x')
+
+    def _make_buttons(self):
+        outer_frm = ttk.Frame(self.main_frm)
+        outer_frm.pack()
+
+        frm = ttk.Frame(outer_frm)
+        frm.pack()
+
+        buttons_in_row = 0
+
+        for caption in self.button_captions:
+            if buttons_in_row == self.MAX_BUTTONS_PER_ROW:
+                frm = ttk.Frame(outer_frm)
+                frm.pack()
+                buttons_in_row = 0
+
+            btn = ttk.Button(frm, text=caption)
+            btn.pack(side='left')
+            buttons_in_row += 1
